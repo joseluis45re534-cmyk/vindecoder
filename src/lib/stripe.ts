b65f4f305fe4c9a84c8573e0c2a3f41673b423ef
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-02-17 09:55:58 +0000

    ERROR IN BUILD

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-01-27.acacia',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    apiVersion: '2025-01-27.acacia' as any,
     httpClient: Stripe.createFetchHttpClient(),
 });
